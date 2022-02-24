85b204bf4bd9fcd4274b2c36def794ff1b87254d
Hanchon <guillermo.paoletti@gmail.com>
2022-02-24 16:00:17 +0100

    feat: init repo

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+const { pathsToModuleNameMapper } = require('ts-jest')
+const { compilerOptions } = require('./tsconfig')
+
+module.exports = {
+  // A list of reporter names that Jest uses when writing coverage reports
+  coverageReporters: ['json', 'html'],
+  // An array of directory names to be searched recursively up from the requiring module's location
+  moduleDirectories: ['.', 'node_modules', 'src'],
+  // An array of file extensions your modules use
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper(
+      compilerOptions.paths /* , { prefix: '<rootDir>/' }, */,
+    ),
+  },
+  // A preset that is used as a base for Jest's configuration
+  preset: 'ts-jest',
+  // The test environment that will be used for testing
+  testEnvironment: 'node',
+}
---
+++ packages/address-converter/src/converter.ts
@@ -0,0 +1,78 @@
+import {
+  isValidChecksumAddress,
+  stripHexPrefix,
+  toChecksumAddress,
+} from 'crypto-addr-codec'
+
+import { bech32 } from 'bech32'
+
+function makeChecksummedHexDecoder(chainId?: number) {
+  return (data: string) => {
+    const stripped = stripHexPrefix(data)
+    if (
+      !isValidChecksumAddress(data, chainId || null) &&
+      stripped !== stripped.toLowerCase() &&
+      stripped !== stripped.toUpperCase()
+    ) {
+      throw Error('Invalid address checksum')
+    }
+    return Buffer.from(stripHexPrefix(data), 'hex')
+  }
+}
+
+function makeChecksummedHexEncoder(chainId?: number) {
+  return (data: Buffer) =>
+    toChecksumAddress(data.toString('hex'), chainId || null)
+}
+
+const hexChecksumChain = (name: string, chainId?: number) => ({
+  decoder: makeChecksummedHexDecoder(chainId),
+  encoder: makeChecksummedHexEncoder(chainId),
+  name,
+})
+
+export const ETH = hexChecksumChain('ETH')
+
+function makeBech32Encoder(prefix: string) {
+  return (data: Buffer) => bech32.encode(prefix, bech32.toWords(data))
+}
+
+function makeBech32Decoder(currentPrefix: string) {
+  return (data: string) => {
+    const { prefix, words } = bech32.decode(data)
+    if (prefix !== currentPrefix) {
+      throw Error('Unrecognised address format')
+    }
+    return Buffer.from(bech32.fromWords(words))
+  }
+}
+
+const bech32Chain = (name: string, prefix: string) => ({
+  decoder: makeBech32Decoder(prefix),
+  encoder: makeBech32Encoder(prefix),
+  name,
+})
+
+export const ETHERMINT = bech32Chain('ETHERMINT', 'ethm')
+
+export const ethToEthermint = (ethAddress: string) => {
+  const data = ETH.decoder(ethAddress)
+  return ETHERMINT.encoder(data)
+}
+
+export const ethermintToEth = (ethermintAddress: string) => {
+  const data = ETHERMINT.decoder(ethermintAddress)
+  return ETH.encoder(data)
+}
+
+export const EVMOS = bech32Chain('EVMOS', 'evmos')
+
+export const ethToEvmos = (ethAddress: string) => {
+  const data = ETH.decoder(ethAddress)
+  return EVMOS.encoder(data)
+}
+
+export const evmosToEth = (evmosAddress: string) => {
+  const data = EVMOS.decoder(evmosAddress)
+  return ETH.encoder(data)
+}
