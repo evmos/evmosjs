5446ca4e6fc027c6d26d5fce598ba1a5d1480e54
Guillermo Paoletti <guillermo.paoletti@gmail.com>
2022-03-09 15:19:37 +0100

    feat: create the evmosjs package on npm (#22)

---
+++ packages/evmosjs/src/index.ts
@@ -0,0 +1,7 @@
+import * as addressConverter from '@tharsis/address-converter'
+import * as eip712 from '@tharsis/eip712'
+import * as proto from '@tharsis/proto'
+import * as provider from '@tharsis/provider'
+import * as transactions from '@tharsis/transactions'
+
+export { addressConverter, eip712, proto, provider, transactions }
