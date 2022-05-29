2d6325e7184f33bf99d728edc243e43c86fcc642
hanchon <guillermo.paoletti@gmail.com>
2022-05-29 18:07:15 +0200

    feat: (wip)withdrawValidatorCommission message

--- packages/eip712/src/messages/staking.ts
+++ packages/eip712/src/messages/staking.ts
@@ -118,3 +118,23 @@ export function createMsgWithdrawDelegatorReward(
     },
   }
 }
+
+export const MSG_WITHDRAW_VALIDATOR_COMMISSION_TYPES = {
+  MsgValue: [{ name: 'validator_address', type: 'string' }],
+}
+
+export interface MsgWithdrawValidatorCommissionInterface {
+  type: string
+  value: {
+    validator_address: string
+  }
+}
+
+export function createMsgWithdrawValidatorCommission(validatorAddress: string) {
+  return {
+    type: 'cosmos-sdk/MsgWithdrawValidatorCommission',
+    value: {
+      validator_address: validatorAddress,
+    },
+  }
+}
