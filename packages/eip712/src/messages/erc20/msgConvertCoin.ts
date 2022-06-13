27608c58507cafb4dd8469e632fca734f3fd44b4
Guillermo Paoletti <guillermo.paoletti@gmail.com>
2022-06-13 16:52:43 +0200

    Update packages/eip712/src/messages/erc20/msgConvertCoin.ts
    Co-authored-by: Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>

--- packages/eip712/src/messages/erc20/msgConvertCoin.ts
+++ packages/eip712/src/messages/erc20/msgConvertCoin.ts
@@ -16,7 +16,7 @@ export function createMsgConvertCoin(
   sender: string,
 ) {
   return {
-    type: 'cosmos-sdk/MsgConvertCoin',
+    type: 'evmos/MsgConvertCoin',
     value: {
       coin: {
         denom,
