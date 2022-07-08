9124a2febd00e60eb9aa8604daf9d4f39e271487
Tien Nam Dao <tien.dao2@tiki.vn>
2022-07-08 14:40:02 +0700

    feat: vesting module (eip712)

--- packages/eip712/src/messages/vesting.ts
+++ packages/eip712/src/messages/vesting.ts
@@ -1,19 +1,36 @@
+interface Coin {
+  denom: string
+  amount: string
+}
+
+interface Period {
+  length: string
+  amount: Coin[]
+}
+
 export const MSG_VESTING_TYPES = {
   MsgValue: [
     { name: 'from_address', type: 'string' },
     { name: 'to_address', type: 'string' },
-    { name: 'start_time', type: 'Timestamp' },
-    { name: 'vesting_periods', type: 'Period[]' },
-    { name: 'merge', type: 'bool' },
-    { name: 'lockup_periods', type: 'Period[]' },
+    { name: 'start_time', type: 'string' },
+    { name: 'lockup_periods', type: 'TypeLockupPeriods[]' },
+    { name: 'vesting_periods', type: 'TypeVestingPeriods[]' },
   ],
-  Timestamp: [
-    { name: 'nanos', type: 'int32' },
-    { name: 'seconds', type: 'int64' },
+  TypeLockupPeriods: [
+    { name: 'length', type: 'int64' },
+    { name: 'amount', type: 'TypeLockupPeriodsAmount[]' },
   ],
-  Period: [
-    { name: 'length', type: 'string' },
-    { name: 'amount', type: 'Coin[]' },
+  TypeVestingPeriods: [
+    { name: 'length', type: 'int64' },
+    { name: 'amount', type: 'TypeVestingPeriodsAmount[]' },
+  ],
+  TypeVestingPeriodsAmount: [
+    { name: 'denom', type: 'string' },
+    { name: 'amount', type: 'string' },
+  ],
+  TypeLockupPeriodsAmount: [
+    { name: 'denom', type: 'string' },
+    { name: 'amount', type: 'string' },
   ],
 }
 
@@ -21,31 +38,17 @@ export function createMsgVesting(
   fromAddress: string,
   toAddress: string,
   startTime: string,
-  vestingPeriods: {
-    length: string
-    amount: { denom: string; amount: string }[]
-  }[],
-  // lockupPeriods?: {
-  //   length: string
-  //   amount: { denom: string; amount: string }[]
-  // }[],
-  merge: boolean = false,
+  lockupPeriods: Period[],
+  vestingPeriods: Period[],
 ) {
-  const time = new Date(startTime).getTime()
-  const tp = {
-    nanos: (time % 1000) * 1e6,
-    seconds: Math.round(time / 1000),
-  }
-
   return {
-    type: 'evmos.vesting.v1.MsgCreateClawbackVestingAccount',
+    type: 'astra/MsgCreateClawbackVestingAccount',
     value: {
       from_address: fromAddress,
       to_address: toAddress,
-      start_time: tp,
-      lockup_periods: [],
+      start_time: startTime,
+      lockup_periods: lockupPeriods,
       vesting_periods: vestingPeriods,
-      merge,
     },
   }
 }
