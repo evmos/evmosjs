ec48413902c96d90871c9c0b3b3cabe47f0b1dc4
Tien Nam Dao <tien.dao2@tiki.vn>
2022-07-22 10:07:31 +0700

    feat(vesting): add multiple messeage vesting

--- packages/transactions/src/messages/vesting.ts
+++ packages/transactions/src/messages/vesting.ts
@@ -1,6 +1,7 @@
 import {
   createMsgVesting as protoMsgVesting,
   createTransaction,
+  createTransactionWithMultipleMessages,
 } from '@astradefi/proto'
 
 import {
@@ -10,6 +11,7 @@ import {
   generateTypes,
   createMsgVesting,
   MSG_VESTING_TYPES,
+  generateMessageWithMultipleTransactions,
 } from '@astradefi/eip712'
 
 import { Chain, Fee, Sender } from './common'
@@ -80,6 +82,77 @@ export function createMessageVesting(
 
   const tx = createTransaction(
     msgVesting,
+    memo,
+    fee.amount,
+    fee.denom,
+    parseInt(fee.gas, 10),
+    'ethsecp256',
+    sender.pubkey,
+    sender.sequence,
+    sender.accountNumber,
+    chain.cosmosChainId,
+  )
+
+  return {
+    signDirect: tx.signDirect,
+    legacyAmino: tx.legacyAmino,
+    eipToSign,
+  }
+}
+
+export function createMultipleMessageVestings(
+  chain: Chain,
+  sender: Sender,
+  fee: Fee,
+  memo: string,
+  params: VestingSendParams[],
+) {
+  /**
+   * EIP712
+   */
+  const feeObject = generateFee(
+    fee.amount,
+    fee.denom,
+    fee.gas,
+    sender.accountAddress,
+  )
+  const types = generateTypes(MSG_VESTING_TYPES)
+
+  const msgs = params.map((param) =>
+    createMsgVesting(
+      sender.accountAddress,
+      param.destinationAddress,
+      param.startTime,
+      param.lockupPeriod,
+      param.vestingPeriod,
+    ),
+  )
+
+  const messages = generateMessageWithMultipleTransactions(
+    sender.accountNumber.toString(),
+    sender.sequence.toString(),
+    chain.cosmosChainId,
+    memo,
+    feeObject,
+    msgs,
+  )
+  const eipToSign = createEIP712(types, chain.chainId, messages)
+
+  /**
+   * Cosmos
+   */
+  const msgVestings = params.map((param) =>
+    protoMsgVesting(
+      sender.accountAddress,
+      param.destinationAddress,
+      param.startTime,
+      param.lockupPeriod,
+      param.vestingPeriod,
+    ),
+  )
+
+  const tx = createTransactionWithMultipleMessages(
+    msgVestings,
     memo,
     fee.amount,
     fee.denom,
