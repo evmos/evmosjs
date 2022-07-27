01e357647bd7612a7b76c8d8e7ab913f8fe8f5af
Tien Nam Dao <tien.dao2@tiki.vn>
2022-07-27 15:55:52 +0700

    feat: authz for Staking Authorization added

---
+++ packages/transactions/src/messages/authz/revoke.ts
@@ -0,0 +1,47 @@
+import {
+  createTransaction,
+  createMsgRevoke,
+  RevokeMessages,
+} from '@astradefi/proto'
+
+import { Chain, Fee, Sender } from '../common'
+
+/* eslint-disable camelcase */
+export interface MsgStakeRevokeAuthorizationParams {
+  bot_address: string
+}
+
+export function createTxMsgStakeRevokeAuthorization(
+  chain: Chain,
+  sender: Sender,
+  fee: Fee,
+  memo: string,
+  params: MsgStakeRevokeAuthorizationParams,
+) {
+  // EIP712
+  // This is blocked until EvmosV7 is released with the eip712 any messages fixes!
+
+  // Cosmos
+  const msgCosmos = createMsgRevoke(
+    sender.accountAddress,
+    params.bot_address,
+    RevokeMessages.REVOKE_MSG_DELEGATE,
+  )
+  const tx = createTransaction(
+    msgCosmos,
+    memo,
+    fee.amount,
+    fee.denom,
+    parseInt(fee.gas, 10),
+    'ethsecp256',
+    sender.pubkey,
+    sender.sequence,
+    sender.accountNumber,
+    chain.cosmosChainId,
+  )
+
+  return {
+    signDirect: tx.signDirect,
+    legacyAmino: tx.legacyAmino,
+  }
+}
