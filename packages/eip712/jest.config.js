27e8c694d055a1c3bf806c28bf42e81fbce86463
Austin Chandra <austinchandra@berkeley.edu>
2022-08-29 16:24:16 -0700

    fix: update global jest config
    Remove moduleDirectories to allow recursive imports within node_modules.

--- packages/eip712/jest.config.js
+++ packages/eip712/jest.config.js
@@ -1,9 +1,5 @@
-// Use babel-jest to solve issue with import within dependency
-// (See https://stackoverflow.com/questions/58613492/how-to-resolve-cannot-use-import-statement-outside-a-module-in-jest)
+const baseConfig = require('../../jest.config.js')
+
 module.exports = {
-  preset: 'ts-jest',
-  transform: {
-    '^.+\\.(ts|tsx)?$': 'ts-jest',
-    '^.+\\.(js|jsx)$': 'babel-jest',
-  },
+  ...baseConfig,
 }
