68fab996175b5d9b6e1c5c6f799526254e9fdde3
Austin Chandra <austinchandra@berkeley.edu>
2022-09-19 09:29:48 -0700

    fix: update chain_id regex matching

--- packages/eip712/src/encoding/utils.ts
+++ packages/eip712/src/encoding/utils.ts
@@ -1,11 +1,17 @@
 // Parse Chain ID string into number. Throws error on failure case.
 export function parseChainId(chainId: string): number {
-  const matches = chainId.match('_(.*)-')
+  const matches = chainId.match(
+    '^([a-z]{1,})_{1}([1-9][0-9]*)-{1}([1-9][0-9]*)$',
+  )
 
-  // Regex matches both _[chain id]- and [chain_id]
-  if (!matches || matches.length !== 2 || Number.isNaN(Number(matches[1]))) {
+  if (
+    !matches ||
+    matches.length !== 4 ||
+    matches[1] === '' ||
+    Number.isNaN(Number(matches[2]))
+  ) {
     throw new Error(`Invalid chainId received: ${chainId}`)
   }
 
-  return Number(matches[1])
+  return Number(matches[2])
 }
