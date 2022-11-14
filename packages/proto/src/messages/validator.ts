b0285d20dfbcfd19a13a7ebd7428aed57a849839
hanchon <guillermo.paoletti@gmail.com>
2022-11-14 12:43:22 +0100

    feat: createValidator message

--- packages/proto/src/messages/validator.ts
+++ packages/proto/src/messages/validator.ts
@@ -1,6 +1,9 @@
-import * as stakingtypes from '../proto/cosmos/staking/v1beta1/staking'
+import * as stakingTypes from '../proto/cosmos/staking/v1beta1/staking'
 import * as staking from '../proto/cosmos/staking/v1beta1/tx'
+import * as coin from '../proto/cosmos/base/v1beta1/coin'
 import * as distribution from '../proto/cosmos/distribution/v1beta1/tx'
+
+import { createAnyMessage, createPubKey } from './utils'
 
 const NOT_MODIFY = '[do-not-modify]'
 
@@ -20,7 +23,7 @@ export function createMsgEditValidator(
   minSelfDelegation: string | undefined,
 ) {
   const message = new staking.cosmos.staking.v1beta1.MsgEditValidator({
-    description: new stakingtypes.cosmos.staking.v1beta1.Description({
+    description: new stakingTypes.cosmos.staking.v1beta1.Description({
       moniker: moniker || NOT_MODIFY,
       identity: identity || NOT_MODIFY,
       website: website || NOT_MODIFY,
@@ -35,6 +38,68 @@ export function createMsgEditValidator(
   return {
     message,
     path: 'cosmos.staking.v1beta1.MsgEditValidator',
+  }
+}
+
+export interface MsgCreateValidatorInterface {
+  path: string
+  message: staking.cosmos.staking.v1beta1.MsgCreateValidator
+}
+
+export function createMsgCreateValidator(
+  validatorDescription: {
+    moniker: string
+    identity: string
+    website: string
+    securityContact: string
+    details: string
+  },
+  validatorCommission: {
+    rate: string
+    maxRate: string
+    maxChangeRate: string
+  },
+  minSelfDelegation: string,
+  delegatorAddress: string,
+  validatorAddress: string,
+  amount: string,
+  denom: string,
+  pubkey: string,
+) {
+  const pubkeyEncoded = new Uint8Array(Buffer.from(pubkey, 'base64'))
+
+  const commission = new stakingTypes.cosmos.staking.v1beta1.CommissionRates({
+    rate: validatorCommission.rate,
+    max_rate: validatorCommission.maxRate,
+    max_change_rate: validatorCommission.maxChangeRate,
+  })
+
+  const description = new stakingTypes.cosmos.staking.v1beta1.Description({
+    moniker: validatorDescription.moniker,
+    identity: validatorDescription.identity,
+    website: validatorDescription.website,
+    security_contact: validatorDescription.securityContact,
+    details: validatorDescription.details,
+  })
+
+  const value = new coin.cosmos.base.v1beta1.Coin({
+    denom,
+    amount,
+  })
+
+  const message = new staking.cosmos.staking.v1beta1.MsgCreateValidator({
+    min_self_delegation: minSelfDelegation,
+    delegator_address: delegatorAddress,
+    validator_address: validatorAddress,
+    value,
+    pubkey: createAnyMessage(createPubKey(pubkeyEncoded)),
+    description,
+    commission,
+  })
+
+  return {
+    message,
+    path: 'cosmos.staking.v1beta1.MsgCreateValidator',
   }
 }
 
