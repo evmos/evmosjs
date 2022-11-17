b2e3874e7d5f89cab336e255ddd25d4b52c9f716
Tien Nam Dao <tien.dao2@tiki.vn>
2022-11-17 15:28:43 +0700

    feat: support for MsgSetWithdrawAddress

--- packages/eip712/src/messages/validator.ts
+++ packages/eip712/src/messages/validator.ts
@@ -51,3 +51,24 @@ export function createMsgEditValidator(
     },
   }
 }
+
+/* eslint-disable camelcase */
+export const MSG_SET_WITHDRAW_ADDRESS_TYPES = {
+  MsgValue: [
+    { name: 'delegator_address', type: 'string' },
+    { name: 'withdraw_address', type: 'string' },
+  ],
+}
+
+export function createMsgSetWithdrawAddress(
+  delegatorAddress: string,
+  withdrawAddress: string,
+) {
+  return {
+    type: 'cosmos-sdk/MsgModifyWithdrawAddress',
+    value: {
+      delegator_address: delegatorAddress,
+      withdraw_address: withdrawAddress,
+    },
+  }
+}
