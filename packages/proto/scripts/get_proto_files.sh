67ae2e97d1a90091179b1aad49be985b3041c2b9
ramacarlucho <ramirocarlucho@gmail.com>
2023-01-10 11:28:39 -0300

    fix(proto): change order of clone

--- packages/proto/scripts/get_proto_files.sh
+++ packages/proto/scripts/get_proto_files.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 MYFOLDER=$(pwd)
 
+cd /tmp
+git clone https://github.com/cosmos/cosmos-sdk/
+cd cosmos-sdk/
+git checkout v0.46.7
+cd third_party/proto
+buf export buf.build/cosmos/cosmos-sdk:8cb30a2c4de74dc9bd8d260b1e75e176 --output .
+cd ../..
+cp -r ./proto/* $MYFOLDER/proto
+cp -r ./third_party/proto/* $MYFOLDER/proto
+cd /tmp
+rm -rf cosmos-sdk
+
 # Ethermint
 mkdir -p proto
 cd /tmp
@@ -18,15 +30,3 @@ git checkout v10.0.0
 cp -r ./proto/* $MYFOLDER/proto
 cd /tmp
 rm -rf evmos
-
-cd /tmp
-git clone https://github.com/cosmos/cosmos-sdk/
-cd cosmos-sdk/
-git checkout v0.46.7
-cd third_party/proto
-buf export buf.build/cosmos/cosmos-sdk:8cb30a2c4de74dc9bd8d260b1e75e176 --output .
-cd ../..
-cp -r ./proto/* $MYFOLDER/proto
-cp -r ./third_party/proto/* $MYFOLDER/proto
-cd /tmp
-rm -rf cosmos-sdk
