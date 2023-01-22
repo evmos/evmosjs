34bba779775bfca0e2fe88639b2ae7c5e33f5a65
hanchon <guillermo.paoletti@gmail.com>
2023-01-22 18:47:52 +0100

    feat: support for generic grants

---
+++ packages/eip712/src/messages/authz/generic.ts
@@ -0,0 +1,45 @@
+export const MSG_GENERIC_AUTHORIZATION_TYPES = {
+  MsgValue: [
+    { name: 'granter', type: 'string' },
+    { name: 'grantee', type: 'string' },
+    { name: 'grant', type: 'TypeGrant' },
+  ],
+  TypeGrant: [
+    { name: 'authorization', type: 'TypeGrantAuthorization' },
+    { name: '"expiration"', type: 'string' },
+  ],
+  TypeGrantAuthorization: [
+    { name: 'type', type: 'string' },
+    { name: 'value', type: 'TypeGrantAuthorizationValue' },
+  ],
+  TypeGrantAuthorizationValue: [{ name: 'msg', type: 'string' }],
+}
+
+export function createMsgGenericAuthorization(
+  sender: string,
+  botAddress: string,
+  typeUrl: string,
+  expires: number,
+) {
+  const date = new Date()
+  date.setTime(expires * 1000)
+  let time = date.toISOString()
+  time = time.replace('.000Z', 'Z')
+
+  return {
+    type: 'cosmos-sdk/MsgGrant',
+    value: {
+      grant: {
+        authorization: {
+          type: 'cosmos-sdk/GenericAuthorization',
+          value: {
+            msg: typeUrl,
+          },
+        },
+        expiration: time,
+      },
+      grantee: botAddress,
+      granter: sender,
+    },
+  }
+}
