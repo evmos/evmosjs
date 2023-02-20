fd16133a68a21af3e33e8501bc499d365b804518
Austin Chandra <austinchandra@berkeley.edu>
2023-02-19 17:57:48 -0800

    fix: refactor MsgClawback

---
+++ packages/transactions/src/messages/vesting/clawback.ts
@@ -0,0 +1,54 @@
+import { createMsgClawback as protoMsgClawback } from '@evmos/proto'
+import { generateTypes, createMsgClawback, MSG_CLAWBACK } from '@evmos/eip712'
+import { createTransactionPayload, TxContext } from '../base'
+
+export interface MsgClawbackParams {
+  funderAddress: string
+  accountAddress: string
+  destAddress?: string
+}
+
+const createEIP712MsgClawback = (params: MsgClawbackParams) => {
+  const types = generateTypes(MSG_CLAWBACK)
+
+  const message = createMsgClawback(
+    params.funderAddress,
+    params.accountAddress,
+    params.destAddress,
+  )
+
+  return {
+    types,
+    message,
+  }
+}
+
+const createCosmosMsgClawback = (params: MsgClawbackParams) => {
+  return protoMsgClawback(
+    params.funderAddress,
+    params.accountAddress,
+    params.destAddress,
+  )
+}
+
+/**
+ * Creates a transaction for a MsgClawback object.
+ *
+ * @remarks
+ * This method creates a transaction wrapping the Evmos
+ * {@link https://docs.evmos.org/modules/vesting/04_transactions.html#clawback | MsgClawback}
+ *
+ * @param context Transaction Context
+ * @param params MsgClawback Params
+ * @returns Transaction with the MsgClawback payload
+ *
+ */
+export const createTxMsgClawback = (
+  context: TxContext,
+  params: MsgClawbackParams,
+) => {
+  const typedData = createEIP712MsgClawback(params)
+  const cosmosMsg = createCosmosMsgClawback(params)
+
+  return createTransactionPayload(context, typedData, cosmosMsg)
+}
