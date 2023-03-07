890394220b86d73673444efc06f20b49f0190ab5
tom <tomasguerraalda@hotmail.com>
2023-03-07 11:00:11 -0300

    feat(erc20): add register coin msg test

--- packages/proto/src/messages/erc20/erc20.spec.ts
+++ packages/proto/src/messages/erc20/erc20.spec.ts
@@ -1,11 +1,16 @@
+import { Metadata } from '@buf/cosmos_cosmos-sdk.bufbuild_es/cosmos/bank/v1beta1/bank_pb'
 import { createMsgConvertCoin } from './msgConvertCoin'
 import { createMsgConvertERC20 } from './msgConvertERC20'
 import { createMsgRegisterERC20 } from './msgRegisterERC20'
+import { createMsgRegisterCoin } from './msgRegisterCoin'
 
 import { MsgConvertCoin, MsgConvertERC20 } from '../../proto/evmos/erc20/tx'
-import { RegisterERC20Proposal } from '../../proto/evmos/erc20/erc20'
+import {
+  RegisterERC20Proposal,
+  RegisterCoinProposal,
+} from '../../proto/evmos/erc20/erc20'
 
-import { from, to, denom, hex } from '../../proto/tests/utils'
+import { from, to, denom, hex, ibcDenom } from '../../proto/tests/utils'
 import { JSONOptions } from '../../proto/tests/common'
 
 describe('test ERC20 Module message generation', () => {
@@ -47,5 +52,45 @@ describe('test ERC20 Module message generation', () => {
       erc20addresses: [hex],
     })
     expect(msg.path).toStrictEqual(RegisterERC20Proposal.typeName)
+  })
+
+  it('correctly wraps msgRegisterCoin', () => {
+    const title = 'Register Test IBC Coin'
+    const metadata = new Metadata({
+      description: 'This is one IBC coin',
+      denomUnits: [
+        { denom: ibcDenom, exponent: 0, aliases: ['stuosmo'] },
+        { denom: 'stosmo', exponent: 6, aliases: [] },
+      ],
+      base: ibcDenom,
+      display: 'stosmo',
+      name: 'Stride Staked Osmo',
+      symbol: 'stOSMO',
+      uri: '',
+      uriHash: '',
+    })
+    const msg = createMsgRegisterCoin(title, title, [metadata])
+
+    // expected output uses snake_case naming instead of camelCase
+    const expMeta = {
+      description: 'This is one IBC coin',
+      denom_units: [
+        { denom: ibcDenom, exponent: 0, aliases: ['stuosmo'] },
+        { denom: 'stosmo', exponent: 6, aliases: [] },
+      ],
+      base: ibcDenom,
+      display: 'stosmo',
+      name: 'Stride Staked Osmo',
+      symbol: 'stOSMO',
+      uri: '',
+      uri_hash: '',
+    }
+
+    expect(msg.message.toJson(JSONOptions)).toStrictEqual({
+      title,
+      description: title,
+      metadata: [expMeta],
+    })
+    expect(msg.path).toStrictEqual(RegisterCoinProposal.typeName)
   })
 })
