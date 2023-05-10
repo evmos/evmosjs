0e621b0ed9f3c3e46fd71b46b8cd34b2128f5fcc
Austin Chandra <austinchandra@berkeley.edu>
2023-05-09 19:10:30 -0700

    feat: add ethsecp256k1 keytype to registry

--- packages/proto/src/messages/crypto/registry.spec.ts
+++ packages/proto/src/messages/crypto/registry.spec.ts
@@ -1,9 +1,13 @@
 import { cryptoRegistryTypes } from './registry'
 
 import { PubKey as ED25519PubKey } from '../../proto/cosmos/crypto/ed25519/keys'
+import { PubKey as EthSecp256k1PubKey } from '../../proto/ethermint/crypto/keys.js'
 
 describe('test crypto registry types against expected', () => {
   it('exactly equals expected types', () => {
-    expect(cryptoRegistryTypes).toStrictEqual([ED25519PubKey])
+    expect(cryptoRegistryTypes).toStrictEqual([
+      ED25519PubKey,
+      EthSecp256k1PubKey,
+    ])
   })
 })
